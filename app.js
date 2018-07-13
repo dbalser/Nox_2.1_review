c79f43e66fd635be8cf848cc328ad1d59537d0ab
Brodie Balser <balserbrodie@gmail.com>
2018-07-13 11:25:58 -0600

    uumoss is commiting something on git

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var app = express();
 
 // Add headers
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Origin", 'http://localhost:3001/');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
