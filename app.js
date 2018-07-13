7496f3449cbaf3b1885ab6130d711580d44a3a97
Brodie Balser <balserbrodie@gmail.com>
2018-07-13 11:03:59 -0600

    uumoss is commiting something on git

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ app.use(function (req, res, next) {
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // // Request headers you wish to allow
-    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-		//
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     // // Set to true if you need the website to include cookies in the requests sent
     // // to the API (e.g. in case you use sessions)
     // res.setHeader('Access-Control-Allow-Credentials', false);
