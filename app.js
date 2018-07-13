b5a7549cec77a04646cd924c73901f2b96499fc0
Brodie Balser <balserbrodie@gmail.com>
2018-07-13 10:46:03 -0600

    uumoss is commiting something on git

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var app = express();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001/');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
