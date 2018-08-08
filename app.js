9272c84c0c86e9cbc2e920643e9f19780a55fa04
Brodie Balser <balserbrodie@gmail.com>
2018-08-07 18:49:08 -0600

    updating cors

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.set('view engine', 'ejs');
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://noxfit.com/');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
